3e411b0196ffbe5daba98a913416b12ec9e718d3
AymenXS <aymenghaloua@gmail.com>
2023-09-30 12:09:41 +0100

    REFACTORING: Database Connection

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 const connectionString =
   'mongodb+srv://Aymen:dRYVBc9sUtBKpBo1@nodeexpressprojects.dgpg94f.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority';
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log('CONNECTED TO THE DB...'))
-  .catch((err) => console.log(err));
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
